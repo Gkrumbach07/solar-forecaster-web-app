f161ddf5f302dc638a4157398d40f80ee2b2c711
Gkrumbach07 <gkrumbac@redhat.com>
2020-07-27 08:38:20 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,168 @@
+import './App.css';
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker } from  "react-leaflet";
+import CanvasJSReact from './canvasjs.react';
+// api_keys
+import env_var from './env_var.json';
+
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+
+function Forecast(props) {
+    const days = props.forecast.map(day => (
+        <tr key={day['observation_time'].value}>
+            <td>{day['observation_time'].value}</td>
+            <td>{((day['temp'][0]['min'].value + day['temp'][1]['max'].value) / 2).toFixed(1)}</td>
+            <td>{day['weather_code'].value}</td>
+        </tr>
+    ))
+
+    return (
+        <table id="forecastTable">
+            <thead>
+                <tr>
+                  <th>Day</th>
+                  <th>Temperature</th>
+                  <th>Forecast</th>
+                </tr>
+            </thead>
+            <tbody>{days}</tbody>
+        </table>
+    );
+}
+
+function MapContainer (props) {
+    const marker = props.marker.hasLocation ? (
+        <Marker position={props.marker.latlng}/>
+    ) : null
+
+    return (
+        <Map
+            center={[34,-108]}
+            zoom={6}
+            onClick={props.onClick}
+            className='leaflet-container'
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {marker}
+        </Map>
+    );
+}
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            'marker': {
+                'hasLocation': false,
+                'latlng': {
+                    'lat': 34,
+                    'lng': -108,
+                }
+            },
+            'forecast': [],
+            'options': {
+                animationEnabled: true,
+                exportEnabled: true,
+                theme: "light2", // "light1", "dark1", "dark2"
+                title:{
+                    text: "Daily Temperature"
+                },
+                axisY: {
+                    title: "Temperature",
+                    includeZero: false,
+                    suffix: "°F"
+                },
+                axisX: {
+                    title: "Date",
+                    interval: 1
+                },
+                data: [{
+                    type: "line",
+                    toolTipContent: "Day {x}: {y}°F",
+                    dataPoints: []
+                }]
+            }
+        };
+
+    }
+
+    handleClick = (e) => {
+        this.setState({'marker': {
+                hasLocation: true,
+                latlng: e.latlng
+            }
+        });
+
+        const api_key = env_var["forecast_api_key"];
+
+        // get weather forecast
+        fetch("https://api.climacell.co/v3/weather/forecast/daily?lat=" + e.latlng.lat + "&lon=" + e.latlng.lng + "&unit_system=us&start_time=now&fields=precipitation%2Cprecipitation_accumulation%2Ctemp%2Cwind_speed%2Cbaro_pressure%2Cvisibility%2Chumidity%2Cweather_code&apikey=" + api_key)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({'forecast': result});
+
+                    //set data points
+                    const dataPoints = this.state.forecast.map((day, i) => (
+                        {
+                            x: i,
+                            y:  parseFloat(((day['temp'][0]['min'].value + day['temp'][1]['max'].value) / 2).toFixed(1))
+                        }
+                    ));
+                    const stateCopy = this.state.options;
+                    stateCopy['data'][0]['dataPoints'] = dataPoints;
+                    this.setState({'options': stateCopy});
+                    this.chart.render();
+                })
+    }
+
+
+    render() {
+        return (
+            <div className="vert-container">
+                <div className="top-container">
+                        <div className="map">
+                            <MapContainer
+                                onClick={e => this.handleClick(e)}
+                                marker={this.state.marker}
+                            />
+                        </div>
+                        <div className="forecast">
+                            <Forecast
+                                forecast={this.state.forecast}
+                            />
+                        </div>
+                </div>
+                <div className="chart">
+                    <CanvasJSChart options={this.state.options} onRef={ref => this.chart = ref}/>
+                </div>
+            </div>
+        );
+    }
+}
+/*
+
+
+ */
+
+export default App;
+
+function f_to_c(t) {
+    return (t - 32) * (5/9);
+}
+
+function c_to_f(t) {
+    return (t * (9/5)) + 32;
+}
+
+// uses C degrees and percent
+function temp_hum_to_dew(temp, humidity) {
+    return (Math.pow((humidity/100), 0.125) * (112 + (0.9 * temp)))
+        + (0.1 * temp)
+        - 112;
+}
