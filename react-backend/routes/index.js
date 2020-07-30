ff04345b3d27582eaec729d91e0126c78ae58591
Gkrumbach07 <gkrumbac@redhat.com>
2020-07-30 15:33:18 -0500

    cleaned up

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+//var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+/* GET users listing. */
+app.get('/users', function(req, res, next) {
+  res.json([
+    {id:1, username:"some"},
+    {id:2, username:"some2"}
+  ]);
 });
 
-module.exports = router;
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
+
+//module.exports = router;
